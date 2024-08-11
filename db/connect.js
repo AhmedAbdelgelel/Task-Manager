cee38c48397525567e6ad92075848b724b8f5c34
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-08-11 15:28:06 +0300

    Init

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
-
-const connectDB = (url) => {
-  mongoose.connect(url);
+require("dotenv").config();
+const DB = process.env.MONGO_URI;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("ERROR: ", error.message);
+  }
 };
-
 module.exports = connectDB;
